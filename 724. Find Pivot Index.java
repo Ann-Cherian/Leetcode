2064219c7c29d0ea2e1860472aef07f41c958861
ann <annbyjus@gmail.com>
2023-01-19 23:23:56 +0530

    724. Find Pivot Index

---
+++ 724. Find Pivot Index.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int pivotIndex(int[] nums) {
+        int sum=0,leftsum=0;
+        for (int i=0;i<nums.length;i++){
+            sum=sum+nums[i];
+        }
+        
+        for(int i=0;i<nums.length;i++){
+        if(leftsum==sum-leftsum-nums[i]){
+            return i;
+        }
+            leftsum=leftsum+nums[i];
+            
+            
+        }
+        return -1;
+        
+    }
+}
+
